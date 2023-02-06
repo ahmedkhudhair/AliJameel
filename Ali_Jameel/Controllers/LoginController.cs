a42461c1a4ee76e4d816621346b9f67153f03dcf
ahmed kudair <ahmedkudair@hotmail.com>
2023-02-06 11:02:33 -0800

    login

--- Ali_Jameel/Controllers/LoginController.cs
+++ Ali_Jameel/Controllers/LoginController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Ali_Jameel.Models;
 
 namespace Ali_Jameel.Controllers
 {
@@ -13,5 +14,45 @@ namespace Ali_Jameel.Controllers
         {
             return View();
         }
+
+        // GET: Login
+        [HttpGet]
+        public ActionResult SignIn(User user)
+        {
+            bool ok = user.SignIn();
+            if (ok)
+            {
+                TempData["Message"] = "Success";
+            }
+            else
+            {
+                TempData["Message"] = "Faild";
+            }
+
+            return RedirectToAction("Index", "Login");
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(User user)
+        {
+            bool exception = false;
+            try
+            {
+                user.SignUp();
+            }
+            catch (Exception ex)
+            {
+                TempData["Message"] = ex.Message;
+                exception = true;
+            }
+
+            if (!exception)
+            {
+                TempData["Message"] = "Rigestered";
+            }
+
+            return RedirectToAction("index", "Login");
+        }
+
     }
 }
